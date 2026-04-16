447a5d679cc39a0955819eb573f496a26081a6ba
Bolice1 <komezusengebolice@gmail.com>
2026-04-16 11:18:01 +0200

    addition of the clear email service

--- server/services/emailService.ts
+++ server/services/emailService.ts
@@ -1,3 +1,11 @@
 import nodemailer from 'nodemailer'
+import { env } from '../config/env'
 
-const transporter 
+const transporter = nodemailer.createTransport({
+    service: env.email_service,
+    auth: {
+        user: env.email_user,
+        pass: env.email_pass
+    }
+}) 
+
