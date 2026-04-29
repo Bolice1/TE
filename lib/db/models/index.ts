78ee18bb45786e10a016d75d474c57ff1d459c9c
Bolice1 <komezusengebolice@gmail.com>
2026-04-29 21:59:26 +0200

    feat: integration of the real time trcking project modern restructuring

--- lib/db/models/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Admin, type IAdmin } from "./admin";
-export { default as Teacher, type ITeacher } from "./teacher";
-export { default as Student, type IStudent } from "./student";
-export { default as Course, type ICourse } from "./course";
-export { default as Quiz, type IQuiz, type QuizType } from "./quiz";
-export { default as Mark, type IMark } from "./mark";
-export { default as ReportCard, type IReportCard } from "./report-card";
-export { default as ActivityLog, type IActivityLog } from "./activity-log";
